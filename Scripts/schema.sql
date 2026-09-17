d63e1e84a4bc279e471774f5921035d419972622
Lucas Ivan Giachero <lucasgiachero1@gmail.com>
2026-09-17 18:49:33 -0300

    Crear esquema inicial de la base de datos y agregar archivos CSV

---
+++ Scripts/schema.sql
@@ -0,0 +1,53 @@
+-- CREO LA TABLA FOODTRUCKS
+CREATE TABLE FoodTrucks (	
+	foodtruck_id INT PRIMARY KEY,
+	name NVARCHAR(100) NOT NULL,
+	cuisine_type NVARCHAR(100) NOT NULL,
+	city NVARCHAR(100) NOT NULL
+);
+
+
+
+
+--CREO LA TABLA PRODUCTS
+
+CREATE TABLE Products (	
+	product_id INT PRIMARY KEY,
+	foodtruck_id INT FOREIGN KEY REFERENCES FoodTrucks(foodtruck_id),
+	name NVARCHAR(100) NOT NULL,
+	category NVARCHAR(100) NOT NULL,
+	price DECIMAL(10,2) CHECK (price>0)
+);
+
+
+-- CREO LA TABLA ORDERS
+
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    foodtruck_id INT FOREIGN KEY REFERENCES FoodTrucks(foodtruck_id),
+    order_date DATE NOT NULL,
+    status NVARCHAR(100) NOT NULL,
+    total DECIMAL(10,2) CHECK (total > 0)
+);
+
+
+
+-- CREO LA TABLA ORDER_ITEMS
+
+CREATE TABLE Order_Items (
+    order_item_id INT PRIMARY KEY,
+    order_id INT FOREIGN KEY REFERENCES Orders(order_id),
+    product_id INT FOREIGN KEY REFERENCES Products(product_id),
+    quantity INT NOT NULL CHECK (quantity > 0)
+);
+
+
+-- CREO LA TABLA LOCATIONS
+
+CREATE TABLE Locations (
+    location_id INT PRIMARY KEY,
+    foodtruck_id INT FOREIGN KEY REFERENCES FoodTrucks(foodtruck_id),
+    date DATE NOT NULL,
+    zone NVARCHAR(100) NOT NULL
+);
+
